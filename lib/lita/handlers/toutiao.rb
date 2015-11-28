60addc21d1f81cbcfaa0a23590b532cd01a0d9a5
easonhan <nbkhic@qq.com>
2015-11-28 20:27:51 +1300

    first version

--- lib/lita/handlers/toutiao.rb
+++ lib/lita/handlers/toutiao.rb
@@ -1,7 +1,39 @@
+require 'nokogiri'
+
 module Lita
   module Handlers
     class Toutiao < Handler
-      # insert handler code here
+
+      route(/toutiao/, :fetch_toutiao, command: true, help: {
+        "v2ex" => "display 10 hot posts of v2ex today"
+      })
+
+      def fetch_toutiao response
+        posts = redis.get(key_of_today)
+        posts = access_api unless posts
+        response.reply posts
+      end
+
+      def key_of_today
+        'toutiao_' + Time.now.strftime('%Y-%m-%d')
+      end
+
+      def access_api
+        url = 'http://toutiao.io/'
+        res = http.get url
+        content = parse_data(res.body)
+        redis.set(key_of_today, content) 
+        content
+      end
+
+      def parse_data(html)
+        posts = []
+        doc = ::Nokogiri::HTML(html)
+        doc.css('.daily .title > a').each_with_index do |post, index|
+          posts << "#{index + 1}.#{post.text.strip}: (#{post[:href]})"
+        end
+        posts.join("\n")
+      end
 
       Lita.register_handler(self)
     end
--- spec/lita/handlers/toutiao_spec.rb
+++ spec/lita/handlers/toutiao_spec.rb
@@ -1,4 +1,11 @@
 require "spec_helper"
 
 describe Lita::Handlers::Toutiao, lita_handler: true do
+  it {is_expected.to route_command("toutiao").to(:fetch_toutiao)}
+
+  it 'should get content form toutiao' do
+    send_command('toutiao')
+    #puts replies.last
+    expect(replies).not_to be_empty
+  end
 end
